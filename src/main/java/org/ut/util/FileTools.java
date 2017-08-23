2252afadad26396d2445883c078c3a105c6f8e72
Felipe Cano <felipecanol@gmail.com>
2017-08-23 07:19:35 -0500

    Se implementó el método saveFile en la clase FileTools

--- src/main/java/org/ut/util/FileTools.java
+++ src/main/java/org/ut/util/FileTools.java
@@ -4,6 +4,7 @@ import java.io.*;
 import java.util.*;
 import java.util.logging.Level;
 import java.util.zip.*;
+
 import org.apache.commons.fileupload.FileItem;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -35,23 +36,23 @@ public class FileTools {
     }
 
     public static void saveFile(MultipartFile file, String path) throws IOException {
-            String realFileName;
-            String fileName = file.getName();
-            if (fileName.lastIndexOf(File.separator) >= 0) {
-                realFileName = path + File.separatorChar
-                        + fileName.substring(fileName.lastIndexOf(File.separatorChar));
-            } else {
-                realFileName = path + File.separatorChar
-                        + fileName.substring(fileName.lastIndexOf(File.separatorChar) + 1);
-            }
-            File f = new File(path + File.separatorChar + file.getName() + ".zip");
-            ZipOutputStream o = new ZipOutputStream(new FileOutputStream(f));
-            ZipEntry e = new ZipEntry(realFileName);
-            o.putNextEntry(e);
-            byte[] data = file.getBytes();
-            o.write(data, 0, data.length);
-            o.closeEntry();
-            o.close();
+        String realFileName;
+        String fileName = file.getName();
+        if (fileName.lastIndexOf(File.separator) >= 0) {
+            realFileName = path + File.separatorChar
+                    + fileName.substring(fileName.lastIndexOf(File.separatorChar));
+        } else {
+            realFileName = path + File.separatorChar
+                    + fileName.substring(fileName.lastIndexOf(File.separatorChar) + 1);
+        }
+        File f = new File(path + File.separatorChar + file.getName() + ".zip");
+        ZipOutputStream o = new ZipOutputStream(new FileOutputStream(f));
+        ZipEntry e = new ZipEntry(realFileName);
+        o.putNextEntry(e);
+        byte[] data = file.getBytes();
+        o.write(data, 0, data.length);
+        o.closeEntry();
+        o.close();
     }
 
     public static File[] getFilesFolder(String folderPath) {

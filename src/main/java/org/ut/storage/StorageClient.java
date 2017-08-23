64775d3f5453bfb53bd3053b55b9958a151fe893
Felipe Cano <felipecanol@gmail.com>
2017-08-23 07:13:22 -0500

    Se implementó la carga de los archivos de configuración y la instanciación del Driver correspondiente, se implementó el metodo storage del cliente

--- src/main/java/org/ut/storage/StorageClient.java
+++ src/main/java/org/ut/storage/StorageClient.java
@@ -1,14 +1,23 @@
 package org.ut.storage;
 
 import org.apache.commons.io.FilenameUtils;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.web.multipart.MultipartFile;
 import org.ut.driver.DLocalFiles;
 import org.ut.driver.Driver;
+import org.ut.driver.DriverFactory;
+import org.ut.util.ConfigTools;
 import org.ut.util.FileTools;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class StorageClient {
@@ -20,33 +29,34 @@ public class StorageClient {
     private Map<String, Driver> drivers;
 
     public static StorageClient getInstance() {
-        if(instance == null) {
+        if (instance == null) {
             instance = new StorageClient();
         }
         return instance;
     }
 
     public StorageClient() {
-        URL configPath = this.getClass().getClassLoader().getResource("/");
-        File[] cfglist = FileTools.getFilesFolder(configPath.getPath());
-        if (cfglist != null) {
-            for (int i = 0; i < cfglist.length; i++) {
-                if (cfglist[i].isFile() && FilenameUtils.getExtension(cfglist[i].getName()) == "properties" && cfglist[i].getName() != "application.properties") {
-                    System.out.println("FILE=" + cfglist[i].getName());
-                    System.out.println("PATH=" + cfglist[i].getAbsolutePath());
+        this.drivers = new HashMap<>();
+        PathMatchingResourcePatternResolver a = new PathMatchingResourcePatternResolver();
+        try {
+            Resource[] r = a.getResources("*");
+            for (int i = 0; i < r.length; i++) {
+                if (FilenameUtils.getExtension(r[i].getFilename()).equals("properties") && !r[i].getFilename().equals("application.properties")) {
+                    Driver d = DriverFactory.getDriver(r[i].getInputStream());
+                    this.drivers.put(d.getName(), d);
                 }
             }
+        } catch (IOException e) {
+            LOGGER.log(Level.SEVERE, "Error: (StorageClient.StorageClient.IOException) " + e.getMessage(), e);
         }
-
-        System.out.println("URL=" + configPath.toString());
     }
 
     public boolean store(MultipartFile file, String path, String driver) {
-        if (drivers.containsKey(driver)) {
-            Driver d = drivers.get(driver);
+        if (this.drivers.containsKey(driver)) {
+            Driver d = this.drivers.get(driver);
             return d.store(file, path);
         } else {
-
+            LOGGER.log(Level.SEVERE, "Error: (StorageClient.store.DriverNotFound) " + driver);
         }
         return false;
     }
